01fd9fd13b11f8bd1ca1abc005d9322be5d21cf6
MURATA Mitsuharu <hikari.photon+mygit@gmail.com>
2020-01-22 09:46:19 +0900

    new method

--- Main.java
+++ Main.java
@@ -4,8 +4,18 @@ public class Main{
         Card cards[];
         
         cards = Poker.getCards();
-        
+
+        // cards[0].n = 1;
+        // cards[1].n = 2;
+        // cards[2].n = 3;
+        // cards[3].n = 4;
+        // cards[4].n = 5;
+
         Card.printCards(cards);
+        System.out.println(Poker.onepair(cards));
         System.out.println(Poker.twopair(cards));
+        System.out.println(Poker.threeCards(cards));
+        System.out.println(Poker.fullHouse(cards));
+        System.out.println(Poker.straight(cards));
     }
 }
--- Poker.java
+++ Poker.java
@@ -1,12 +1,68 @@
-public class Poker{
+class Poker{
     public static
-    boolean twopair(Card card[]){
+    boolean onepair(Card cards[]){
         int[] n = new int[13];
-        for(int i = 0; i < card.length; i++)
-            n[card[i].n] += 1;
+        for(int i = 0; i < cards.length; i++)
+            n[cards[i].n] += 1;
         for(int i = 0; i < 13; i++)
             if(n[i] >= 2)
                 return true;
+        return false;
+    }
+
+    public static
+    boolean twopair(Card cards[]){
+        int[] n = new int[13];
+        boolean flag = false;
+        for(int i = 0; i < cards.length; i++)
+            n[cards[i].n] += 1;
+        for(int i = 0; i < 13; i++)
+            if(n[i] >= 2){
+                if(flag) return true;
+                flag = true;
+            }
+        return false;
+    }
+
+    public static
+    boolean threeCards(Card cards[]){
+        int[] n = new int[13];
+        for(int i = 0; i < cards.length; i++)
+            n[cards[i].n] += 1;
+        for(int i = 0; i < 13; i++)
+            if(n[i] >= 3)
+                return true;
+        return false;
+    }
+
+    public static
+    boolean fullHouse(Card cards[]){
+        int[] n = new int[13];
+        boolean flag1 = false;
+        boolean flag2 = false;
+        for(int i = 0; i < cards.length; i++)
+            n[cards[i].n] += 1;
+        for(int i = 0; i < 13; i++){
+            if(n[i] == 2) flag1 = true;
+            if(n[i] == 3) flag2 = true;
+            if(flag1 && flag2) return true;
+        }
+        return false;
+    }
+
+    public static
+    boolean straight(Card[] cards){
+        int[] n = new int[13];
+        int i, j;
+        for(i = 0; i < cards.length; i++)
+            n[cards[i].n] += 1;
+        for(i = 0; i < 13; i++)
+            if(n[i] == 1) break;
+        if(n[i] == 1){
+            for(j = 1; j < 5; j++)
+                if(n[i + j] != 1) return false;
+            return true;
+        }
         return false;
     }
 
